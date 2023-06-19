4e110fd77d0df30c196fe2ea04ac47860bd4066a
AaronCaneRWT <93556860+AaronCaneRWT@users.noreply.github.com>
2023-06-19 04:11:14 -0600

    First commit

--- src/main.py
+++ src/main.py
@@ -1,14 +1,5 @@
 from adapter.easybroker import apiConsult
 
-'''
-cd C:/Users/aaron/OneDrive/Escritorio/EasyBroker
-.\env\Scripts\activate
-set EASYBROKER_CLIENT_ID=l7u502p8v46ba3ppgvj5y2aad50lb9
-set URL=https://api.stagingeb.com/v1/
-cd src
-'''
-
-
 if __name__ == '__main__':
     print('Starting...')
     responses=apiConsult.get_organization_properties()
